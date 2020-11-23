b2d15163ab12f2e253edfd6af79b3d995cae7ffd
김 미성 <rlaaltjd46@jinhaksa.net>
2020-11-23 12:00:50 +0900

    입출력 끝

--- 10992.py
+++ 10992.py
@@ -2,10 +2,9 @@ import sys
 N = int(sys.stdin.readline().rstrip())
 x = ""
 for i in range(1, N+1) :
-  for j in range(1, N+i-1) :
-    if (j == N+i-1) : 
-      x += "*"
-    elif (i > 1 and j == N-i-1) : 
+  x = ""
+  for j in range(1, N+i) :
+    if (j == N+i-1 or i > 1 and j == (N-i+1) or i == N) : 
       x += "*"
     else :
       x += " "
