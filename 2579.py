d0b35c3d83e63961ab91271fb17a2221bacf83b4
김 미성 <rlaaltjd46@jinhaksa.net>
2021-01-04 19:12:37 +0900

    2579 품

--- 2579.py
+++ 2579.py
@@ -4,6 +4,14 @@ A = []
 for i in range(n) :
   A.append(int(sys.stdin.readline().rstrip()))
 
-dp = [A[n - 1]]
-for i in range(n - 2, -1, -1) :
-  dp.append()
+dp = [A[0]]
+
+for i in range(1, n) :
+  if i == 1 : 
+    dp.append(A[0] + A[1])
+  elif i == 2 :
+    dp.append(max(A[0] + A[2], A[1] + A[2]))
+  else : 
+    dp.append(max(dp[i - 2] + A[i], dp[i - 3] + A[i-1] + A[i]))
+
+print(dp[n-1])
