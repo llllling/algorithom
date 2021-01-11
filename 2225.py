fcd0185fe52f9acc0c8d16b2ccba9484ad07d97d
MISEONGKIM <MISEONGKIM@github.com>
2021-01-11 18:46:16 +0900

    2225까지

--- 2225.py
+++ 2225.py
@@ -1,8 +1,16 @@
 import sys
 [N, K] = list(map(int, sys.stdin.readline().rstrip().split()))
-dp = [[1] for _ in range(N + 1)]
+dp = []
+for i in range(0, N + 1):
+    dp.append([0 ,1])
+    for j in range(2, K + 1) :
+        if i == 0 : 
+            dp[i].append(1)
+            continue
 
-for i in range(1, N + 1):
-  for j in range(2, K) :
-    dp[i].append(dp[i][j - 1] + dp[i - j][j - 1])
+        dp[i].append(dp[i][j - 1])
+
+        for x in range(0, i) :
+            dp[i][j] += dp[x][j - 1]
     
+print(dp[N][K] % 1000000000)
