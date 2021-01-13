dfb21dc2b27f6974bdeee0f52d62a9c38e975329
MISEONGKIM <MISEONGKIM@github.com>
2021-01-13 19:53:29 +0900

    11052 까지

--- 11052.py
+++ 11052.py
@@ -1,3 +1,10 @@
 import sys
 N = int(sys.stdin.readline().rstrip())
-P = int(sys.stdin.readline().rstrip().split())
+P = list(map(int, sys.stdin.readline().rstrip().split()))
+
+dp = [P[0]]
+for i in range(1, N) :
+    dp.append(P[i])
+    for j in range(i // 2 + 1) :
+        dp[i] = max(dp[i], dp[j] + dp[i - j - 1])
+print(dp[N-1])
