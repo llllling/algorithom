e30a984bda9d959640fc76d6135000f5799bffab
김 미성 <rlaaltjd46@jinhaksa.net>
2021-01-21 18:01:46 +0900

    10825까지

--- 10825.py
+++ 10825.py
@@ -1,12 +1,12 @@
 import sys
 N = int(sys.stdin.readline().rstrip())
 A = []
+
 for i in range(N) :
   t = sys.stdin.readline().rstrip().split()
-  A.append([int(t[0]), i, t[1]])
+  A.append([t[0], int(t[1]), int(t[2]), int(t[3])])
 
-A.sort()
+result = sorted(A, key = lambda x : (-x[1], x[2], -x[3], x[0]))
 
 for i in range(N) :
-  print(A[i][0], A[i][2])
-
+  print(result[i][0])
