a48000f24fb250cb9b61ef386ac4569b90c30acc
MISEONGKIM <daesung0413@naver.com>
2021-02-17 11:57:41 +0900

    ee

--- 11724.py
+++ 11724.py
@@ -1,4 +1,6 @@
 import sys
+import queue
+
 N, M = map(int, sys.stdin.readline().rstrip().split())
 g = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
 
@@ -6,3 +8,17 @@ for i in range(M) :
     u, v = map(int, sys.stdin.readline().rstrip().split())
     g[u][v] = 1
     g[v][u] = 1
+
+def DFS(graph, start) :
+    visited = []
+    need_visit = queue.LifoQueue()
+
+    need_visit.put(start)
+
+    while len(need_visit) > 0 :
+        v = need_visit.get()
+
+        if v not in visited :
+            visited.append(v)
+            for i in graph[v] :
+                need_visit.put(graph[v])
